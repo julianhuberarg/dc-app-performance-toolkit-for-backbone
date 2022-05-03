520a7bf29e450d0df8904b81169be6a60dd758fe
Julian Huber <julianh@k15t.com>
2022-05-03 11:33:28 +0200

    test changes

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -33,14 +33,14 @@ def app_specific_action(webdriver, datasets):
 #     measure()
 #
     @print_timing("selenium_app_custom_action")
-        def measure():
-            @print_timing("selenium_app_custom_action:view_issue")
-            def sub_measure():
-                page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-                page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-                page.wait_until_visible((By.ID, "bb-sync-info-panel"))  # Wait for you app-specific UI element by ID selector
-            sub_measure()
-        measure()
+    def measure():
+        @print_timing("selenium_app_custom_action:view_issue")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
+            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
+            page.wait_until_visible((By.ID, "bb-sync-info-panel"))  # Wait for you app-specific UI element by ID selector
+        sub_measure()
+    measure()
 #
 #
 #     @print_timing("Open_Settings_UI_for_Backbone")
